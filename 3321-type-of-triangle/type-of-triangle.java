7df9e1bde74137b4f4f697148bd1a6be7304a621
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-05-19 11:47:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (43.93%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -1,6 +1,6 @@
 class Solution {
     public String triangleType(int[] nums) {
-         int sum=nums[1]+nums[2];
+        int sum=nums[1]+nums[2];
         int sum2=nums[0]+nums[1];
         int sum3=nums[0]+nums[2];
      if(nums[0]>=sum){
