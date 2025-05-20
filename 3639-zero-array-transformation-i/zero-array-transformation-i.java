b115c5f6a38a78d5975d4abda0af09f28c5f2fd7
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-05-20 12:42:02 +0530

    Time: 4 ms (13.33%) | Memory: 79.7 MB (94.76%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
         int diff[]=new int[nums.length];
-        int k=0;
         for(int arr[]:queries){
             int lef=arr[0];
-            int righ=arr[1];
-           
+            int rig=arr[1];
+
             diff[lef]+=1;
-            if(righ+1 < nums.length){
-                diff[righ+1]+=-1;
+            if(rig+1 < nums.length){
+                diff[rig+1]-=1;
             }
         }
-        int result[]=new int[nums.length];
-        result[0]=diff[0];
-        for(int i=1;i<result.length;i++){
-            result[i]=result[i-1]+diff[i];
+        // 2 step prefix_sum;
+        int ans[]=new int[nums.length];
+        ans[0]=diff[0];
+        for(int i=1;i<nums.length;i++){
+            ans[i]=ans[i-1]+diff[i];
         }
-        for(int i=0;i<result.length;i++){
-            if(result[i] < nums[i]){
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>ans[i]){
                 return false;
             }
         }
