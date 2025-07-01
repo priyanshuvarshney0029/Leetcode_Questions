603ec005a28ab0ff96a448987bd7266b8b66e19b
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-07-01 08:36:30 +0530

    Time: 2 ms (65.52%) | Memory: 44.5 MB (39.46%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+       if(prices[0]+prices[1]<=money){
+          return money=money-prices[0]-prices[1];
+       }
+       else{
+         return money;
+       }
+        
+    }
+}
