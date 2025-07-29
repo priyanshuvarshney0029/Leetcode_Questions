b797a5a7d58364aa6ae7cad8c078685c6a02a80f
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-07-29 22:51:43 +0530

    Time: 10 ms (35.34%) | Memory: 41.4 MB (46.55%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,7 +1,12 @@
 class Solution {
     public int countMaxOrSubsets(int[] nums) {
-        int maxOR = findMaxOR(nums,nums.length-1);
-        int []count = new int[1];
+        // int maxOR = findMaxOR(nums,nums.length-1);
+        int []count = new int[1]; 
+        int maxOR=0;
+        for(int i=0;i<nums.length;i++){
+             maxOR|=nums[i];
+        }
+        // for avoding static variable........
         countArr(nums,nums.length-1,0,count,maxOR);
         return count[0];
     }
@@ -15,12 +20,12 @@ class Solution {
         countArr(nums,idx-1,OR,count,maxOR);
         countArr(nums,idx-1,OR|nums[idx],count,maxOR);
     }
-    public int findMaxOR(int []nums,int idx){
-        if(idx<0){
-            return 0;
-        }
-        int not_pick = findMaxOR(nums,idx-1);
-        int pick = (nums[idx] | findMaxOR(nums,idx-1));
-        return Math.max(pick,not_pick);
-    }
+    // public int findMaxOR(int []nums,int idx){
+    //     if(idx<0){
+    //         return 0;
+    //     }
+    //     int not_pick = findMaxOR(nums,idx-1);
+    //     int pick = (nums[idx] | findMaxOR(nums,idx-1));
+    //     return Math.max(pick,not_pick);
+    // }
 }
