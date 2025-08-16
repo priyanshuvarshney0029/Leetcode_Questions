c72f69f832e9bf558fefc9a729ab669ec59d3c47
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-08-16 23:51:45 +0530

    Time: 239 ms (5.00%) | Memory: 58.8 MB (17.42%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,12 +1,29 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        int count=1;
+        TreeMap<Integer,Boolean> map=new TreeMap<>();
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==count){
-                count++;
+            map.put(nums[i],true);
+            if(map.containsKey(nums[i]+1)){
+                map.put(nums[i]+1,false);
+            }
+            if(map.containsKey(nums[i]-1)){
+                map.put(nums[i],false);
+            }
+        }
+        int count=1;
+        for(int key:map.keySet()){
+            int i=key;
+            if(!map.get(key)){
+                continue;
+            }
+            while(map.containsKey(i)){
+                if(i==count){
+                    count++;
+                }
+                i++;
             }
         }
         return count;
+
     }
 }
