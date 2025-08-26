3ee295830373254b224132baf906dac320f913a3
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-08-26 09:34:26 +0530

    Time: 5 ms (92.78%) | Memory: 44.5 MB (42.95%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,21 +1,37 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int dp[]=new int[nums.length];
-        Arrays.fill(dp,1);
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i] > nums[j]){
-                   int left=dp[j];
-                   int right=dp[i];
-                   right=Math.max(dp[i],left+1);
-                   dp[i]=right;
-                }
-            }
-        }
-        int max=0;
-        for(int i=0;i<dp.length;i++){
-            max=Math.max(max,dp[i]);
-        }
-        return max;
+         ArrayList<Integer> ll=new ArrayList<>();
+         ll.add(nums[0]);
+         for(int i=1;i<nums.length;i++){
+              if(nums[i] > ll.get(ll.size()-1)){
+                  ll.add(nums[i]);
+              }
+              else{
+                 int idx=Bs(nums[i],ll);
+                 ll.set(idx,nums[i]);
+              }
+         }
+         return ll.size();
+    }
+    public static int Bs(int target,ArrayList<Integer> ll){
+         int low=0;
+         int high=ll.size()-1;
+         int idx=0;
+
+         while(low<=high){
+             int mid=(low+high)/2;
+
+             if(ll.get(mid) >= target){
+                   idx=mid;
+                   high=mid-1;
+             }
+             else{
+                low=mid+1;
+             }
+
+
+
+         }
+         return idx;
     }
 }
