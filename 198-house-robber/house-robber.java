6c99f0dd79adaa0c6e8e883a9561685884cb52ac
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-09-27 17:00:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (35.72%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,30 @@
 class Solution {
     public int rob(int[] nums) {
         int dp[]=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return solve(nums,0,dp);
+        // Arrays.fill(dp,-1);
+        return bottom_up(nums,dp);
     }
-    public static int solve(int nums[],int idx,int dp[]){
-        if(idx>=nums.length){
+    public static int solve(int nums[],int idx){
+        if(idx<0){
             return 0;
-        }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int chori=nums[idx]+solve(nums,idx+2,dp);
-        int not_Chori=solve(nums,idx+1,dp);
 
-        return dp[idx]=Math.max(chori,not_Chori);
+        }
+        int chori=nums[idx]+solve(nums,idx-2);
+        int not_chori=solve(nums,idx-1);
+
+        return Math.max(chori,not_chori);
+    }
+    public static int bottom_up(int nums[],int dp[]){
+         if(nums.length==1){
+            return nums[0];
+         }
+         dp[0]=nums[0];
+         dp[1]=Math.max(nums[0],nums[1]);
+         for(int i=2;i<dp.length;i++){
+            int chori=nums[i]+dp[i-2];
+            int not_Chori=dp[i-1];
+            dp[i]=Math.max(chori,not_Chori);
+         }
+         return dp[dp.length-1];
     }
 }
