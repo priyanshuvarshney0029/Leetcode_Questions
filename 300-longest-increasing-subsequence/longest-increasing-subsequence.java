e150a6e6818488f1a8bc2622f6f6036e48806ae3
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-09-27 23:02:17 +0530

    Time: 52 ms (34.61%) | Memory: 44.1 MB (88.84%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,37 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-         ArrayList<Integer> ll=new ArrayList<>();
-         ll.add(nums[0]);
-         for(int i=1;i<nums.length;i++){
-              if(nums[i] > ll.get(ll.size()-1)){
-                  ll.add(nums[i]);
-              }
-              else{
-                 int idx=Bs(nums[i],ll);
-                 ll.set(idx,nums[i]);
-              }
-         }
-         return ll.size();
-    }
-    public static int Bs(int target,ArrayList<Integer> ll){
-         int low=0;
-         int high=ll.size()-1;
-         int idx=0;
-
-         while(low<=high){
-             int mid=(low+high)/2;
-
-             if(ll.get(mid) >= target){
-                   idx=mid;
-                   high=mid-1;
-             }
-             else{
-                low=mid+1;
-             }
-
-
-
-         }
-         return idx;
+        int []lis=new int[nums.length];
+        Arrays.fill(lis,1);
+        for(int i=1;i<lis.length;i++){
+            for(int j=0; j < i;j++){
+                if(nums[j] < nums[i]){
+                    int ll=lis[j];
+                    lis[i]=Math.max(lis[i],ll+1);
+                }
+            }
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<lis.length;i++){
+            max=Math.max(max,lis[i]);
+        }
+        return max;
+        
     }
 }
