819a52e8657974cf1e7823f98b4e4ceea4ee18ed
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-10-01 06:33:17 +0530

    Time: 16 ms (22.18%) | Memory: 55.2 MB (5.16%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,38 +1,27 @@
 class Solution {
     public int change(int amount, int[] coins) {
+          return solve(coins,amount);
+    }
+    public static int solve(int coins[],int amount){
         int dp[][]=new int[amount+1][coins.length+1];
-
-        // abse case fill
+        
         for(int i=0;i<dp[0].length;i++){
-            dp[0][i]=1;
+            dp[0][i]=1;    // base case
         }
-        for(int i=1;i<dp.length;i++){
+
+        for(int i=1;i<dp.length;i++){ // amount
             for(int j=1;j<dp[0].length;j++){
-              int  non_pick=dp[i][j-1];
-              int pick=0;
-              if(i>=coins[j-1]){
-                   pick=dp[i-coins[j-1]][j];
+                int non_pick=dp[i][j-1];
+                int pick=0;
+
+                if(i >= coins[j-1]){
+                    pick=dp[i-coins[j-1]][j];
                 }
                 dp[i][j]=pick+non_pick;
             }
         }
         return dp[dp.length-1][dp[0].length-1];
 
+    }
 
-        // return solve(amount,coins,coins.length-1);
-    }
-    public int solve(int amt,int coins[],int idx){
-        if(amt==0){
-            return 1;
-        }
-        if(idx<0){      //idx==coins.length // ye bhi kar skte h
-           return 0;
-        }
-        int pick=0,non_pick=0;
-        if(amt>=coins[idx]){
-            pick=solve(amt-coins[idx],coins,idx);
-        }
-        non_pick=solve(amt,coins,idx-1);
-        return pick+non_pick;
-    }
 }
