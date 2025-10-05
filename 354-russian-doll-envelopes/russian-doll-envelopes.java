44c5142debf8bcece2a8279ab8d4035b25b3ff1d
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-10-05 10:35:42 +0530

    Time: 45 ms (59.99%) | Memory: 101.4 MB (74.56%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,40 +1,34 @@
 class Solution {
-    public int maxEnvelopes(int[][] envelopes) {
-        // first sort the width..
-        // lambda function...
-        // if width equal sort them on the basis of hegiht in des order // otherwise simple sort width..
-        Arrays.sort(envelopes,(a,b) -> a[0]==b[0] ? b[1]-a[1] : a[0]-b[0]);
+    public int maxEnvelopes(int[][] arr) {
+        Arrays.sort(arr,(a,b)-> a[0]==b[0] ? b[1]-a[1] : a[0]-b[0]);
         ArrayList<Integer> ll=new ArrayList<>();
-        // applying lis on height.....
-        ll.add(envelopes[0][1]);
-        for(int i=1;i<envelopes.length;i++){
-            if(envelopes[i][1] > ll.get(ll.size()-1)){
-                ll.add(envelopes[i][1]);
+        // apply lis on heights
+        ll.add(arr[0][1]);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i][1] > ll.get(ll.size()-1)){
+                ll.add(arr[i][1]);
             }
             else{
-                int cp=binarysearch(ll,envelopes[i][1]);
-                ll.set(cp,envelopes[i][1]);
-
+                int cp=bs(ll,arr[i][1]);
+                ll.set(cp,arr[i][1]);
             }
+        }
+        return ll.size();
     }
-     return ll.size();
-}
-    public int binarysearch(ArrayList<Integer> ll,int target){
+    public static int bs(ArrayList<Integer> ll,int target){
         int ans=0;
-        int lo=0;
+        int low=0;
         int high=ll.size()-1;
-
-        while(lo<=high){
-            int mid=lo+(high-lo)/2;
-            if(ll.get(mid)>=target){
+        while(low <= high){
+            int mid=low+(high-low)/2;
+            if(ll.get(mid) >= target){
                 ans=mid;
                 high=mid-1;
             }
             else{
-                lo=mid+1;
+                low=mid+1;
             }
         }
         return ans;
     }
-        
 }
