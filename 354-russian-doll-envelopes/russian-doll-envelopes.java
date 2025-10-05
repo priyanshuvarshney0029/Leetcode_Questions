f3be6bd3c88424d8943d41bfd9f95c190b184a70
Priyanshu_07 <114874909+priyanshuvarshney0029@users.noreply.github.com>
2025-10-05 09:37:58 +0530

    Time: 44 ms (61.97%) | Memory: 101.3 MB (76.61%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,36 +1,40 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
+        // first sort the width..
+        // lambda function...
+        // if width equal sort them on the basis of hegiht in des order // otherwise simple sort width..
         Arrays.sort(envelopes,(a,b) -> a[0]==b[0] ? b[1]-a[1] : a[0]-b[0]);
         ArrayList<Integer> ll=new ArrayList<>();
+        // applying lis on height.....
         ll.add(envelopes[0][1]);
-
         for(int i=1;i<envelopes.length;i++){
             if(envelopes[i][1] > ll.get(ll.size()-1)){
                 ll.add(envelopes[i][1]);
             }
             else{
-                int cp=bs(ll,envelopes[i][1]);
+                int cp=binarysearch(ll,envelopes[i][1]);
                 ll.set(cp,envelopes[i][1]);
+
             }
-        }
-        return ll.size();
     }
-    public static int bs(ArrayList<Integer> ll,int target){
-        int ans=ll.size();
-        int low=0;
+     return ll.size();
+}
+    public int binarysearch(ArrayList<Integer> ll,int target){
+        int ans=0;
+        int lo=0;
         int high=ll.size()-1;
 
-        while(low <= high){
-            int mid=low+(high-low)/2;
-            if(ll.get(mid) >= target){
+        while(lo<=high){
+            int mid=lo+(high-lo)/2;
+            if(ll.get(mid)>=target){
                 ans=mid;
                 high=mid-1;
-
             }
             else{
-                low=mid+1;
+                lo=mid+1;
             }
         }
         return ans;
     }
+        
 }
